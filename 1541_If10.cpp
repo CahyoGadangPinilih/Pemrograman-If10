8563b69b00b4fe861c53be4ac1173b2d3c1edd57
CahyoGadangPinilih <41361987+CahyoGadangPinilih@users.noreply.github.com>
2018-07-19 02:29:50 +0700

    Add files via upload
    Aplikasi Daftar Nama Kota Besar di Indonesia
    Nama : Cahyo Gadang Pinilih
    NIM :17.11.1541
    Kelas : S1Informatika-10

---
+++ 1541_If10.cpp
@@ -0,0 +1,438 @@
+#include<iostream>
+using namespace std;
+
+class Indonesia
+{
+	public:
+		int kota;
+
+		public:
+			void Aceh()
+	{
+		cout<<"\n\tKota Besar:";
+		cout<<"\n\t1. Banda Aceh";
+		cout<<"\n\t2. Langsa";
+		cout<<"\n\t3. Meulaboh";
+		cout<<"\n\t4. Sabang";
+		cout<<"\n\t5. Subulussalam";
+		cout<<"\n\n";
+	}
+
+			void Riau()
+	{	
+		cout<<"\n\tKota Besar:";
+		cout<<"\n\t1. Dumai";
+		cout<<"\n\t2. Pekanbaru";
+		cout<<"\n\n";
+	}
+
+			void KepulauanRiau()
+	{	
+		cout<<"\n\tKota Besar:";
+		cout<<"\n\t1. Batam";
+		cout<<"\n\t2. Tanjungpinang";
+		cout<<"\n\n";
+	}
+
+			void SumateraUtara()
+	{
+		cout<<"\n\tKota Besar:";
+		cout<<"\n\t1. Binjai";
+		cout<<"\n\t2. Medan";
+		cout<<"\n\t3. Padang Sidempuan";
+		cout<<"\n\t4. Pematangsiantar";
+		cout<<"\n\t5. Sibolga";
+		cout<<"\n\t6. Tanjungbalai";
+		cout<<"\n\t7. Tebing Tinggi";
+		cout<<"\n\n";
+	}
+
+			void SumateraBarat()
+	{
+		cout<<"\n\tKota Besar:";
+		cout<<"\n\t1. Bukittinggi";
+		cout<<"\n\t2. Padang";
+		cout<<"\n\t3. Padangpanjang";
+		cout<<"\n\t4. Pariaman";
+		cout<<"\n\t5. Payakumbuh";
+		cout<<"\n\t6. Sawahlunto";
+		cout<<"\n\t7. Solok";
+		cout<<"\n\n";
+	}
+
+			void SumateraSelatan()
+	{
+		cout<<"\n\tKota Besar:";
+		cout<<"\n\t1. Lubuklinggau";
+		cout<<"\n\t2. Pagaralam";
+		cout<<"\n\t3. Palembang";
+		cout<<"\n\t4. Prabumulih";
+		cout<<"\n\n";
+	}
+
+			void Bengkulu()
+	{
+		cout<<"\n\tKota Besar:";
+		cout<<"\n\t1. Bengkulu";
+		cout<<"\n\n";
+	}
+
+			void Jambi()
+	{	
+		cout<<"\n\tKota Besar:";
+		cout<<"\n\t1. Jambi";
+		cout<<"\n\t2. Sungai Penuh";
+		cout<<"\n\n";
+	}
+
+			void BangkaBelitung()
+	{
+		cout<<"\n\tKota Besar:";
+		cout<<"\n\t1. Pangkalpinang";
+		cout<<"\n\n";
+	}
+
+			void Lampung()
+	{
+		cout<<"\n\tKota Besar:";
+		cout<<"\n\t1. Bandar Lampung";
+		cout<<"\n\t2. Metro";
+		cout<<"\n\n";
+	}
+
+
+			void Banten()
+	{
+		cout<<"\n\tKota Besar:";
+		cout<<"\n\t1. Cilegon";
+		cout<<"\n\t2. Serang";
+		cout<<"\n\t3. Tangerang";
+		cout<<"\n\t4. Tangerang Selatan";
+		cout<<"\n\n";
+	}
+
+			void Jakarta()
+	{	
+		cout<<"\n\tKota Besar:";
+		cout<<"\n\t1. Jakarta Pusat";
+		cout<<"\n\t2. Jakarta Selatan";
+		cout<<"\n\t3. Jakarta Utara";
+		cout<<"\n\t4. Jakarta Barat";
+		cout<<"\n\t5. Jakarta Timur";
+		cout<<"\n\n";
+	}
+
+			void JawaBarat()
+	{	
+		cout<<"\n\tKota Besar:";
+		cout<<"\n\t1. Bandung";
+		cout<<"\n\t2. Bogor";
+		cout<<"\n\t3. Bekasi";
+		cout<<"\n\t4. Cimahi";
+		cout<<"\n\t5. Cirebon";
+		cout<<"\n\t6. Sukabumi";
+		cout<<"\n\t7. Tasikmalaya";
+		cout<<"\n\t8. Banjar";
+		cout<<"\n\n";
+	}
+
+			void JawaTengah()
+	{	
+		cout<<"\n\tKota Besar:";
+		cout<<"\n\t1. Magelang";
+		cout<<"\n\t2. Pekalongan";
+		cout<<"\n\t3. Purwokerto";
+		cout<<"\n\t4. Salatiga";
+		cout<<"\n\t5. Semarang";
+		cout<<"\n\t6. Surakarta";
+		cout<<"\n\t7. Tegal";
+		cout<<"\n\n";
+	}
+
+			void Yogyakarta()
+	{
+		cout<<"\n\tKota Besar:";
+		cout<<"\n\t1. Yogyakarta";
+		cout<<"\n\n";
+	}
+
+			void JawaTimur()
+	{	
+		cout<<"\n\tKota Besar:";
+		cout<<"\n\t1. Blitar";
+		cout<<"\n\t2. Batu";
+		cout<<"\n\t3. Kediri";
+		cout<<"\n\t4. Madiun";
+		cout<<"\n\t5. Malang";
+		cout<<"\n\t6. Mojokerto";
+		cout<<"\n\t7. Pasuruan";
+		cout<<"\n\t8. Probolinggo";
+		cout<<"\n\t9. Surabaya";
+		cout<<"\n\t10. Madura";
+		cout<<"\n\n";
+	}
+
+
+			void Bali()
+	{
+		cout<<"\n\tKota Besar:";
+		cout<<"\n\t1. Denpasar";
+		cout<<"\n\n";
+	}
+
+
+			void KalimantanSelatan()
+	{	
+		cout<<"\n\tKota Besar:";
+		cout<<"\n\t1. Banjarbaru";
+		cout<<"\n\t2. Banjarmasin";
+		cout<<"\n\n";
+	}
+
+			void KalimantanTengah()
+	{	
+		cout<<"\n\tKota Besar:";
+		cout<<"\n\t1. Palangkaraya";
+		cout<<"\n\n";
+	}
+
+			void KalimantanBarat()
+	{	
+		cout<<"\n\tKota Besar:";
+		cout<<"\n\t1. Pontianak";
+		cout<<"\n\t2. Singkawang";
+		cout<<"\n\n";
+	}
+	
+			void KalimantanTimur()
+	{	
+		cout<<"\n\tKota Besar:";
+		cout<<"\n\t1. Balikpapan";
+		cout<<"\n\t2. Bontang";
+		cout<<"\n\t3. Samarinda";
+		cout<<"\n\n";
+	}
+
+			void KalimantanUtara()
+	{	
+		cout<<"\n\tKota Besar:";
+		cout<<"\n\t1. Tarakan";
+		cout<<"\n\n";
+	}
+	
+	
+			void NTT()
+	{	
+		cout<<"\n\tKota Besar:";
+		cout<<"\n\t1. Kupang";
+		cout<<"\n\n";
+	}
+
+			void NTB()
+	{	
+		cout<<"\n\tKota Besar:";
+		cout<<"\n\t1. Bima";
+		cout<<"\n\t2. Mataram";
+		cout<<"\n\n";
+	}
+
+			void SulawesiSelatan()
+	{
+		cout<<"\n\tKota Besar:";
+		cout<<"\n\t1. Makasar";
+		cout<<"\n\t2. Palopo";
+		cout<<"\n\t3. Parepare";
+		cout<<"\n\n";
+	}
+
+			void SulawesiTengah()
+	{
+		cout<<"\n\tKota Besar:";
+		cout<<"\n\t1. Palu";
+		cout<<"\n\n";
+	}
+
+			void SulawesiTenggara()
+	{
+		cout<<"\n\tKota Besar:";
+		cout<<"\n\t1. Bau-Bau";
+		cout<<"\n\t2. Kendari";
+		cout<<"\n\n";
+	}
+
+			void SulawesiUtara()
+	{
+		cout<<"\n\tKota Besar:";
+		cout<<"\n\t1. Bitung";
+		cout<<"\n\t2. Kotamobagu";
+		cout<<"\n\t3. Manado";
+		cout<<"\n\t4. Tomohon";
+		cout<<"\n\n";
+	}
+
+		void SulawesiBarat()
+	{
+		cout<<"\n\tKota Besar:";
+		cout<<"\n\t1. Mamuju";
+		cout<<"\n\n";
+	}
+
+		void Gorontalo()
+	{
+		cout<<"\n\tKota Besar:";
+		cout<<"\n\t1. Gorontalo";
+		cout<<"\n\n";
+	}
+
+			void Maluku()
+	{
+		cout<<"\n\tKota Besar:";
+		cout<<"\n\t1. Ambon";
+		cout<<"\n\t2. Tual";
+		cout<<"\n\n";
+	}
+ 	
+ 			void MalukuUtara()
+	{
+		cout<<"\n\tKota Besar:";
+		cout<<"\n\t1. Ternate";
+		cout<<"\n\t2. Tidore Kepulauan";
+		cout<<"\n\n";
+	}
+
+
+	 		void PapuaBarat()
+	{
+		cout<<"\n\tKota Besar:";
+		cout<<"\n\t1. Sorong";
+		cout<<"\n\n";
+	}
+
+	 		void Papua()
+	{
+		cout<<"\n\tKota Besar:";
+		cout<<"\n\t1. Jayapura";
+		cout<<"\n\n";
+	}
+};
+
+int main()
+{
+	Indonesia indonesia;
+	cout<<"\n|===================================|";
+	cout<<"\n|   DAFTAR NAMA KOTA DI INDONESIA   |";
+	cout<<"\n|\t   PILIH PROVINSI           |";
+	cout<<"\n|===================================|";
+	cout<<"\n|\t1.  Aceh                    |";
+	cout<<"\n|\t2.  Riau                    |";
+	cout<<"\n|\t3.  Kepulauan Riau          |";
+	cout<<"\n|\t4.  Sumatera Utara          |";
+	cout<<"\n|\t5.  Sumatera Barat          |";
+	cout<<"\n|\t6.  Sumatera Selatan        |";
+	cout<<"\n|\t7.  Bengkulu                |";
+	cout<<"\n|\t8.  Jambi                   |";
+	cout<<"\n|\t9.  Bangka Belitung         |";
+	cout<<"\n|\t10. Lampung                 |";
+	cout<<"\n|\t11. Banten                  |";
+	cout<<"\n|\t12. Jakarta                 |";
+	cout<<"\n|\t13. Jawa Barat              |";
+	cout<<"\n|\t14. Jawa Tengah             |";
+	cout<<"\n|\t15. Yogyakarta              |";
+	cout<<"\n|\t16. Jawa Timur              |";
+	cout<<"\n|\t17. Bali                    |";
+	cout<<"\n|\t18. Kalimantan Selatan      |";
+	cout<<"\n|\t19. Kalimantan Tengah       |";
+	cout<<"\n|\t20. Kalimantan Barat        |";
+	cout<<"\n|\t21. Kalimantan Timur        |";
+	cout<<"\n|\t22. Kalimantan Utara        |";
+	cout<<"\n|\t23. NTT                     |";
+	cout<<"\n|\t24. NTB                     |";
+	cout<<"\n|\t25. Sulawesi Selatan        |";
+	cout<<"\n|\t26. Sulawesi Tengah         |";
+	cout<<"\n|\t27. Sulawesi Tenggara       |";
+	cout<<"\n|\t28. Sulawesi Utara          |";
+	cout<<"\n|\t29. Sulawesi Barat          |";
+	cout<<"\n|\t30. Gorontalo               |";
+	cout<<"\n|\t31. Maluku                  |";
+	cout<<"\n|\t32. Maluku Utara            |";
+	cout<<"\n|\t33. Papua Barat             |";
+	cout<<"\n|\t34. Papua                   |";
+	cout<<"\n|===================================|";
+	cout<<"\n\n\tMasukan Kode Pilihan: ";
+	cin>>indonesia.kota;
+	cout<<"\n====================================\n";
+
+		if(indonesia.kota == 1 )
+			indonesia.Aceh();
+		else if(indonesia.kota == 2 )	
+			indonesia.Riau();
+		else if(indonesia.kota == 3 )		
+			indonesia.KepulauanRiau();
+		else if(indonesia.kota == 4 )
+			indonesia.SumateraUtara();
+		else if(indonesia.kota == 5 )	
+			indonesia.SumateraBarat();
+		else if(indonesia.kota == 6 )	
+			indonesia.SumateraSelatan();
+		else if(indonesia.kota == 7 )	
+			indonesia.Bengkulu();
+		else if(indonesia.kota == 8 )	
+			indonesia.Jambi();
+		else if(indonesia.kota == 9 )	
+			indonesia.BangkaBelitung();
+		else if(indonesia.kota == 10 )	
+			indonesia.Lampung();
+		else if(indonesia.kota == 11 )	
+			indonesia.Banten();
+		else if(indonesia.kota == 12 )	
+			indonesia.Jakarta();
+		else if(indonesia.kota == 13 )	
+			indonesia.JawaBarat();
+		else if(indonesia.kota == 14 )	
+			indonesia.JawaTengah();
+		else if(indonesia.kota == 15 )	
+			indonesia.Yogyakarta();
+		else if(indonesia.kota == 16 )	
+			indonesia.JawaTimur();
+		else if(indonesia.kota == 17 )	
+			indonesia.Bali();
+		else if(indonesia.kota == 18 )	
+			indonesia.KalimantanSelatan();
+		else if(indonesia.kota == 19 )	
+			indonesia.KalimantanTengah();
+		else if(indonesia.kota == 20 )	
+			indonesia.KalimantanBarat();
+		else if(indonesia.kota == 21 )	
+			indonesia.KalimantanTimur();
+		else if(indonesia.kota == 22 )	
+			indonesia.KalimantanUtara();
+		else if(indonesia.kota == 23 )	
+			indonesia.NTT();
+		else if(indonesia.kota == 24 )	
+			indonesia.NTB();
+		else if(indonesia.kota == 25 )
+			indonesia.SulawesiSelatan();
+		else if(indonesia.kota == 26 )	
+			indonesia.SulawesiTengah();
+		else if(indonesia.kota == 27 )	
+			indonesia.SulawesiTenggara();
+		else if(indonesia.kota == 28 )	
+			indonesia.SulawesiUtara();
+		else if(indonesia.kota == 29 )	
+			indonesia.SulawesiBarat();
+		else if(indonesia.kota == 30 )	
+			indonesia.Gorontalo();
+		else if(indonesia.kota == 31 )	
+			indonesia.Maluku();
+		else if(indonesia.kota == 32 )	
+			indonesia.MalukuUtara();
+		else if(indonesia.kota == 33 )	
+			indonesia.PapuaBarat();
+		else if(indonesia.kota == 34 )	
+			indonesia.Papua();
+
+		else
+			cout<<"\n    ~~ MAAF PILIHAN TIDAK ADA ~~\n\n";
+
+				return 0;
+}
